4350506e37e0b3051eada5c5f53a88ca63f39733
Patryk Stępień <fighting.dreamer.0@gmail.com>
2024-03-11 11:22:55 +0100

    Make debug configurable from env variable

--- center_django/settings.py
+++ center_django/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "0") == "1"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
